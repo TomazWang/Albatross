2a2313fdcf4577ce534a0c36f963598be6ac33a5
Tomaz Wang <TomazWang.tw@gmail.com>
2016-09-27 18:47:53 +0800

    - base ListFragment

--- app/src/main/java/me/tomazwang/app/albatross/ListFragment.java
+++ app/src/main/java/me/tomazwang/app/albatross/ListFragment.java
@@ -3,6 +3,8 @@ package me.tomazwang.app.albatross;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -23,6 +25,7 @@ public class ListFragment extends Fragment {
     private int mListID;
 
     private OnInteractionListener mListener;
+    private RecyclerView mRecyclerView;
 
     public ListFragment() {
         // Required empty public constructor
@@ -49,6 +52,16 @@ public class ListFragment extends Fragment {
                              Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.fragment_list, container, false);
+
+        mRecyclerView = (RecyclerView)view.findViewById(R.id.list_task);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+
+        TaskAdapter mTaskAdapter = new TaskAdpter();
+
+
+
+
 
         // TODO: get widgets
 
---
+++ app/src/main/java/me/tomazwang/app/albatross/TaskAdpter.java
@@ -0,0 +1,36 @@
+package me.tomazwang.app.albatross;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by TomazWang on 2016/9/27.
+ */
+
+public class TaskAdpter extends RecyclerView.Adapter<TaskAdpter.ViewHolder>{
+
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+
+        }
+    }
+}
